2fb005be210647fbadc2f52af8777b7a36a3a4fe
YudzhinKr <ikrugovua@gmail.com>
2023-12-04 22:21:30 +0200

    1

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_bsD0tmcbjEWlwMpwXS3uyJjVIxpNF9BJ2jHR16mTBeJPml6nkCfJDB09Ug6pHttz';
 
+// Оголошення змінних для елементів DOM
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
 const breedListURL = 'https://api.thecatapi.com/v1/breeds';
 const catByBreedURL = 'https://api.thecatapi.com/v1/images/search';
 
+// Оголошення функцій для взаємодії з API та відображенням інформації
 function fetchBreeds() {
   return axios
     .get(breedListURL)
@@ -29,51 +31,29 @@ function fetchCatByBreed(breedId) {
 }
 
 function displayBreeds(breeds) {
-  breeds.forEach(breed => {
-    const option = document.createElement('option');
-    option.value = breed.id;
-    option.textContent = breed.name;
-    selectElement.data.appendChild(option);
-  });
+  // Ваш код для відображення списку порід у відповідному елементі DOM
 }
 
 function displayCatInfo(cat) {
-  catInfo.innerHTML = `
-    <img src="${cat.url}" alt="Cat Image" />
-    <p><strong style="color: red;">Breed:</strong> ${cat.breeds[0].name}</p>
-    <p><strong style="color: red;">Description:</strong> ${cat.breeds[0].description}</p>
-    <p><strong style="color: red;">Temperament:</strong> ${cat.breeds[0].temperament}</p>
-  `;
+  // Ваш код для відображення інформації про кота у відповідному елементі DOM
 }
 
 async function loadBreeds() {
   try {
-    loader.style.display = 'block';
-    selectElement.slim.data.innerHTML = '';
-    const breeds = await fetchBreeds();
-    displayBreeds(breeds);
-    loader.style.display = 'none';
+    // Ваш код для завантаження списку порід
   } catch (error) {
-    loader.style.display = 'none';
-    error.style.display = 'block';
-    console.error(error);
+    // Ваш код для обробки помилок при завантаженні списку порід
   }
 }
 
+// Прослуховувач подій на зміну вибору породи
 selectElement.slim.on('change', async () => {
-  const selectedBreedId = selectElement.selected();
   try {
-    catInfo.style.display = 'none';
-    loader.style.display = 'block';
-    const catInfoData = await fetchCatByBreed(selectedBreedId);
-    displayCatInfo(catInfoData[0]);
-    loader.style.display = 'none';
-    catInfo.style.display = 'block';
+    // Ваш код для завантаження інформації про кота за обраною породою
   } catch (error) {
-    loader.style.display = 'none';
-    error.style.display = 'block';
-    console.error(error);
+    // Ваш код для обробки помилок при завантаженні інформації про кота
   }
 });
 
+// Завантаження списку порід при завантаженні сторінки
 window.addEventListener('DOMContentLoaded', loadBreeds);
