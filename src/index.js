cb3672e1617e2cce536057240934696c00531fee
YudzhinKr <ikrugovua@gmail.com>
2023-12-05 20:50:31 +0200

    iteration

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
-
 axios.defaults.headers.common['x-api-key'] =
   'live_bsD0tmcbjEWlwMpwXS3uyJjVIxpNF9BJ2jHR16mTBeJPml6nkCfJDB09Ug6pHttz';
 
-// Оголошення змінних для елементів DOM
+const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
 const breedListURL = 'https://api.thecatapi.com/v1/breeds';
 const catByBreedURL = 'https://api.thecatapi.com/v1/images/search';
 
-// Оголошення функцій для взаємодії з API та відображенням інформації
 function fetchBreeds() {
   return axios
     .get(breedListURL)
@@ -31,29 +29,136 @@ function fetchCatByBreed(breedId) {
 }
 
 function displayBreeds(breeds) {
-  // Ваш код для відображення списку порід у відповідному елементі DOM
+  breeds.forEach(breed => {
+    const option = document.createElement('option');
+    option.value = breed.id;
+    option.textContent = breed.name;
+    breedSelect.appendChild(option);
+  });
 }
 
 function displayCatInfo(cat) {
-  // Ваш код для відображення інформації про кота у відповідному елементі DOM
+  catInfo.innerHTML = `
+    <img src="${cat.url}" alt="Cat Image" />
+    <p><strong style="color: red">Breed:</strong> ${cat.breeds[0].name}</p>
+    <p><strong>Description:</strong> ${cat.breeds[0].description}</p>
+    <p><strong>Temperament:</strong> ${cat.breeds[0].temperament}</p>
+  `;
 }
 
 async function loadBreeds() {
   try {
-    // Ваш код для завантаження списку порід
+    loader.style.display = 'block';
+    const breeds = await fetchBreeds();
+    displayBreeds(breeds);
+    loader.style.display = 'none';
   } catch (error) {
-    // Ваш код для обробки помилок при завантаженні списку порід
+    loader.style.display = 'none';
+    error.style.display = 'block';
   }
 }
 
-// Прослуховувач подій на зміну вибору породи
-selectElement.slim.on('change', async () => {
+breedSelect.addEventListener('change', async event => {
+  const selectedBreedId = event.target.value;
+
   try {
-    // Ваш код для завантаження інформації про кота за обраною породою
+    const catInfoData = await fetchCatByBreed(selectedBreedId);
+    displayCatInfo(catInfoData[0]);
   } catch (error) {
-    // Ваш код для обробки помилок при завантаженні інформації про кота
+    console.error(error);
   }
 });
 
-// Завантаження списку порід при завантаженні сторінки
+window.addEventListener('DOMContentLoaded', loadBreeds);
+
+function showLoader(element) {
+  element.style.display = 'block';
+}
+
+function hideLoader(element) {
+  element.style.display = 'none';
+}
+
+async function loadBreeds() {
+  try {
+    showLoader(loader);
+    breedSelect.style.display = 'none';
+
+    const breeds = await fetchBreeds();
+    displayBreeds(breeds);
+
+    hideLoader(loader);
+    breedSelect.style.display = 'block';
+  } catch (error) {
+    hideLoader(loader);
+    error.style.display = 'block';
+  }
+}
+
+breedSelect.addEventListener('change', async event => {
+  const selectedBreedId = event.target.value;
+
+  try {
+    showLoader(loader);
+    catInfo.style.display = 'none';
+
+    const catInfoData = await fetchCatByBreed(selectedBreedId);
+    displayCatInfo(catInfoData[0]);
+
+    hideLoader(loader);
+    catInfo.style.display = 'block';
+  } catch (error) {
+    hideLoader(loader);
+    console.error(error);
+  }
+});
+
+window.addEventListener('DOMContentLoaded', loadBreeds);
+
+function showErrorMessage() {
+  error.style.display = 'block';
+}
+
+function hideErrorMessage() {
+  error.style.display = 'none';
+}
+
+async function loadBreeds() {
+  try {
+    showLoader(loader);
+    breedSelect.style.display = 'none';
+    hideErrorMessage();
+
+    const breeds = await fetchBreeds();
+    displayBreeds(breeds);
+
+    hideLoader(loader);
+    breedSelect.style.display = 'block';
+  } catch (error) {
+    hideLoader(loader);
+    showErrorMessage();
+    console.error(error);
+  }
+}
+
+breedSelect.addEventListener('change', async event => {
+  const selectedBreedId = event.target.value;
+
+  try {
+    showLoader(loader);
+    catInfo.style.display = 'none';
+    hideErrorMessage();
+
+    const catInfoData = await fetchCatByBreed(selectedBreedId);
+    displayCatInfo(catInfoData[0]);
+
+    hideLoader(loader);
+    catInfo.style.display = 'block';
+  } catch (error) {
+    hideLoader(loader);
+    showErrorMessage();
+    console.error(error);
+  }
+});
+
 window.addEventListener('DOMContentLoaded', loadBreeds);
